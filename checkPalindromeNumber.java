f6edcdaf634e37fe499c5dfe3ad89cdc369ba0d9
Sarthak-Goel <84651609+Sarthak-Goel@users.noreply.github.com>
2022-11-15 17:10:16 +0530

    Create checkPalindromeNumber.java

---
+++ checkPalindromeNumber.java
@@ -0,0 +1,21 @@
+# WAP to check if the number entered is Palindrome
+# x is the test number, which is passed into the check isPalindrome function
+#---------------------------------
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = Integer.toString(x);
+        int len = str.length();
+        for(int i = 0; i < len / 2; i++)
+            if (str.charAt(i) != str.charAt(len - i - 1))
+                return false;
+        return true;
+    }
+}
+
+#--------------------------------
+
+# Algorithm-
+# 1. Convert number to String
+# 2. Use a 'for' Loop to traverse string from both ends and check for equality of characters encountered
+# 3. Return True False, depending on that
